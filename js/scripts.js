cbd0a9708135df30289a4f637d675156ec70c853
mocelinjr <47070332+mocelinjr@users.noreply.github.com>
2023-03-05 11:46:28 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,44 @@
-/*!
-    * Start Bootstrap - Freelancer v6.0.4 (https://startbootstrap.com/themes/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-    */
-    (function($) {
-    "use strict"; // Start of use strict
-  
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 71)
-          }, 1000, "easeInOutExpo");
-          return false;
-        }
-      }
-    });
-  
-    // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 80
-    });
-  
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
+anychart.onDocumentReady(function () {
+    // create polar chart
+    var chart = anychart.polar();
+
+    // create data set on our data
+    chartData = {
+        rows: [
+            ['Jan', 14229],
+            ['Feb', 8814, 5067, 13987, 3932],
+            ['Mar', 11624, 7004, 3574, 5221],
+            ['Apr', 8814, 9054, 4376, 5256],
+            ['May', 9998, 5043, 4572, 3308],
+            ['Jun', 12321, 15067, 3417, 5432],
+            ['Jul', 10342, 10119, 9231, 9701],
+            ['Aug', 4814, 3054, 4376, 4229],
+            ['Sep', 8814, 5067, 3987, 3932],
+            ['Oct', 11624, 17004, 3574, 5221],
+            ['Nov', 8814, 9054, 4376, 9256],
+            ['Dec', 12998, 8814, 4572, 3308]
+        ]
     };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-  
-    // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-      });
-    });
-  
-  })(jQuery); // End of use strict
-  
+
+    // sort data by X
+    chart.sortPointsByX(true)
+    // set series type
+        .defaultSeriesType('column')
+        // disable y-axis
+        .yAxis(false)
+        // set x-scale
+        .xScale('ordinal');
+
+    // set chart data
+    chart.data(chartData);
+    chart.xGrid(false);
+    chart.yGrid(false);
+
+    // set stack mod
+    chart.yScale().stackMode('value');
+
+    // set chart container id
+    chart.container('container');
+    // initiate chart drawing
+    chart.draw();
+});
